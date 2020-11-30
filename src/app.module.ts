aca90e0e1a66e503d90ec7d8fefbcab9975d7775
cpplpp <cappa-lappa@yandex.ru>
2020-12-01 02:37:55 +0300

    adding db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { TypeOrmModule } from "@nestjs/typeorm";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { CatalogModule } from './catalog/catalog.module';
 
 @Module({
-  // imports: [TypeOrmModule.forRoot(), CatalogModule],
-  imports: [CatalogModule],
+  imports: [TypeOrmModule.forRoot(), CatalogModule],
+  // imports: [CatalogModule],
   controllers: [AppController],
   providers: [AppService],
 })
